8d11b268ab945c6e863ed4996bc0d2988fed8511
apavlov <apavlov@igatec.com>
2025-04-17 18:13:18 +0300

    add headers and change port

--- cmd/main.go
+++ cmd/main.go
@@ -7,11 +7,18 @@ import (
 	"2kitchen/internal/services"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "https://2kitchen-frontend.vercel.app",
+		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+	}))
 
 	// dishes
 	rDishes, err := repositories.NewDishRepository("dishes.db")
@@ -31,7 +38,7 @@ func main() {
 	hOrders := handlers.NewOrderHandler(sOrders)
 	routes.SetupOrderRoutes(app, hOrders)
 
-	port := "80"
+	port := "8080"
 	logrus.WithFields(logrus.Fields{
 		"port": port,
 	}).Info("Server starting on port")
