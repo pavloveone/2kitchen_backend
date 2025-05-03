134a91c9f882a9648d3264166d7f204f39ce48aa
apavlov <apavlov@igatec.com>
2025-05-03 17:23:05 +0300

    add auth middlware

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,10 +1,13 @@
 package auth
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/joho/godotenv"
 	"golang.org/x/crypto/bcrypt"
@@ -13,7 +16,7 @@ import (
 var jwtKey []byte
 
 func init() {
-	err := godotenv.Load()
+	err := godotenv.Load("../.env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
@@ -39,7 +42,7 @@ func CheckPasswordHash(password, hashedPassword string) bool {
 
 func GenerateTokens(userId int) (string, string, error) {
 	accesTokenClaims := jwt.MapClaims{
-		"user_id": userId,
+		"user_id": fmt.Sprintf("%d", userId),
 		"exp":     time.Now().Add(15 * time.Minute).Unix(),
 	}
 	accessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, accesTokenClaims)
@@ -48,7 +51,7 @@ func GenerateTokens(userId int) (string, string, error) {
 		return "", "", err
 	}
 	refreshTokenClaims := jwt.MapClaims{
-		"user_id": userId,
+		"user_id": fmt.Sprintf("%d", userId),
 		"exp":     time.Now().Add(7 * 24 * time.Hour).Unix(),
 	}
 	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshTokenClaims)
@@ -57,4 +60,40 @@ func GenerateTokens(userId int) (string, string, error) {
 		return "", "", err
 	}
 	return accessTokenStr, refreshTokenStr, nil
+}
+
+func AuthMiddleware(c *fiber.Ctx) error {
+	authHeader := c.Get("Authorization")
+	if authHeader == "" {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+
+	tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+
+	claims, err := validateToken(tokenStr)
+
+	if err != nil {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+	userId, ok := (*claims)["user_id"].(string)
+	if !ok {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+	c.Locals("user", userId)
+
+	return c.Next()
+}
+
+func validateToken(tokenStr string) (*jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (any, error) {
+		return jwtKey, nil
+	})
+	if err != nil || !token.Valid {
+		return nil, err
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, fmt.Errorf("invalid claims")
+	}
+	return &claims, nil
 }
--- internal/routes/order/routes.go
+++ internal/routes/order/routes.go
@@ -1,6 +1,7 @@
 package orderroutes
 
 import (
+	"2kitchen/internal/auth"
 	orderhandler "2kitchen/internal/handlers/order"
 
 	"github.com/gofiber/fiber/v2"
@@ -8,6 +9,6 @@ import (
 
 func SetupOrderRoutes(app *fiber.App, h *orderhandler.OrderHandler) {
 	ordersGroup := app.Group("/orders")
-	ordersGroup.Get("", h.AllOrders)
+	ordersGroup.Get("", auth.AuthMiddleware, h.AllOrders)
 	ordersGroup.Post("", h.CreateOrder)
 }
